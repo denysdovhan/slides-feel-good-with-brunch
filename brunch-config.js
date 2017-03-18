d97291798a6f898119e96e6ed42a6dc1971379c2
Denys Dovhan <email@denysdovhan.com>
2017-03-18 14:42:55 +0000

    Updated at Sat, 18 Mar 2017, 14:42:55, UTC

---
+++ brunch-config.js
@@ -0,0 +1,32 @@
+module.exports = {
+  files: {
+    javascripts: {
+      joinTo: {
+        'vendor.js': /^(?!app)/,
+        'app.js': /^app/
+      }
+    },
+    stylesheets: {
+      joinTo: 'app.css'
+    },
+    templates: {
+      joinTo: 'templates.js'
+    }
+  },
+
+  paths: {
+    public: 'dist'
+  },
+
+  npm: {
+    styles: {
+      spectacle: ['lib/themes/default/index.css']
+    }
+  },
+
+  modules: {
+    autoRequire: {
+      'app.js': ['index.js']
+    }
+  }
+}
